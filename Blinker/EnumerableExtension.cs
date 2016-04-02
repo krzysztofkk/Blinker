2ac08ebdb547c5bf90d029f046503b522c3eb63d
Krzysztof Kulewski <kkulewski@kuznia.co>
2016-04-03 00:16:17 +0200

    Add some cool extension methods

---
+++ Blinker/EnumerableExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blinker
+{
+	public static class EnumerableExtension
+	{
+		public static T PickRandom<T>(this IEnumerable<T> source)
+		{
+			return source.PickRandom(1).Single();
+		}
+
+		public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+		{
+			return source.Shuffle().Take(count);
+		}
+
+		public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+		{
+			return source.OrderBy(x => Guid.NewGuid());
+		}
+
+		public static void AddMany<T>(this List<T> list, params T[] elements)
+		{
+			list.AddRange(elements);
+		}
+	}
+}
