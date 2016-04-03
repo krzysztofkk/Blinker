ff6b5f34f99eb10ebcc10e3ed81b4461df4b9d7d
Krzysztof Kulewski <kkulewski@kuznia.co>
2016-04-03 02:24:48 +0200

    Update initializer

--- Blinker/Initializer.cs
+++ Blinker/Initializer.cs
@@ -12,7 +12,9 @@ namespace Blinker
 			Writer.WriteInfo("Text");
 			Writer.WriteInfo(" - an information message (i.e room description)\n");
 			Writer.WriteAction("Text");
-			Writer.WriteInfo(" - commited action (i.e picked up item, moved to another location\n");
+			Writer.WriteInfo(" - commited action (i.e picked up item, moved to another location)\n");
+			Writer.WriteActionHostile("Text");
+			Writer.WriteInfo(" - hostile action (i. e fight)\n");
 			Writer.WriteDialog("Text");
 			Writer.WriteInfo(" - a dialog/npc\n");
 			Writer.WriteLocation("Text");
